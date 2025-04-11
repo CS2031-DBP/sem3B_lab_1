e5654c6601bd5f83ad49fc0699c41589f2332bc1
Mateo Noel Rabines <39866695+mateonoel2@users.noreply.github.com>
2025-04-11 18:42:56 -0500

    nueva dependencia

---
+++ src/main/java/com/example/sem3b_lab_1/GlobalConfig.java
@@ -0,0 +1,14 @@
+package com.example.sem3b_lab_1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GlobalConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+}
